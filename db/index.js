5329f0edf47ebe2c105a65fb76b1dd3edb802402
Josh Zappone <zappone.josh@gmail.com>
2020-09-04 22:24:47 -0700

    officifize seed.sql and creates initial view queries

--- db/index.js
+++ db/index.js
@@ -1,6 +1,97 @@
 // require connection
 
 // create DB class with methods for all db queries
+const connection = require('./connection');
+
+class DB {
+    constructor(connection) {
+        this.connection = connection
+    }
+    //DB queries organized by CRUD
+    // viewAllEmployees
+    viewAllEmployees() {
+        return this.connection.query(
+            `
+            SELECT
+                employees.id,
+                employees.first_name AS "First Name",
+                employees.last_name AS "Last Name",
+                roles.title AS Role,
+                department.name AS Department,
+                CONCAT(m.first_name, ' ', m.last_name) AS Manager
+            FROM 
+                employees
+            LEFT JOIN 
+                roles 
+                ON employees.role_id = roles.id
+            LEFT JOIN
+                department
+                ON roles.department_id = department.id
+            LEFT JOIN
+                employees m
+                ON employees.manager_id = m.id
+            `
+        )
+    }
+    // viewRoles
+    viewRoles() {
+        return this.connection.query(
+            `
+            SELECT
+                roles.title AS Role,
+                roles.salary,
+                department.name AS Department
+            FROM 
+                roles
+            LEFT JOIN 
+                department
+                ON roles.department_id = department.id
+            ORDER BY
+                salary DESC
+            `
+        )
+    }
+    // viewDepartments
+    viewDepartments() {
+        return this.connection.query(
+            `
+            SELECT
+                department.name AS Departments
+            FROM department
+            `
+        )
+    }
+    // viewAllEmployeesByDepartment
+    // viewAllEmployeesByManager
+    // viewDepartmentBudgets
+
+    // addEmployee
+    // addRole
+    // addDepartment
+
+    // updateRole
+    // updateEmployeeManager
+
+    // removeEmployee
+    // removeDepartment
+    // removeRole
+
+    
+    
+  
+
+}
+
+// testing queries
+let test = new DB(connection)
+
+async function testMe() {
+    const employees = await test.viewDepartments()
+    console.log(employees)
+}
+
+testMe()
 
 
 module.exports = new DB(connection)
+
--- db/seed.sql
+++ db/seed.sql
@@ -1,32 +1,47 @@
 -- departments
 INSERT INTO department (name)
-VALUES ('food');
+VALUES ('administration');
 
 INSERT INTO department (name)
-VALUES ('clothing');
+VALUES ('accounting');
 
 INSERT INTO department (name)
-VALUES ('home improvement');
+VALUES ('sales');
+
+INSERT INTO department (name)
+VALUES ('warehouse');
+
+-- ROLES
+-- admin
+INSERT INTO roles (title, salary, department_id)
+VALUES ('manager', 75000.00, 1);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ('co-manager', 75000.00, 1);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ('receptionist', 30000.00, 1);
 
 -- leads
 INSERT INTO roles (title, salary, department_id)
-VALUES ('department lead', 50000.00, 1);
+VALUES ('accounting lead', 50000.00, 2);
 
 INSERT INTO roles (title, salary, department_id)
-VALUES ('department lead', 50000.00, 2);
+VALUES ('sales lead', 50000.00, 3);
 
 INSERT INTO roles (title, salary, department_id)
-VALUES ('department lead', 50000.00, 3);
+VALUES ('warehouse lead', 50000.00, 4);
 
--- stockers for each dep
+-- peasants
 INSERT INTO roles (title, salary, department_id)
-VALUES ('food dep stocker', 25000.00, 1);
-
-INSERT INTO roles (title, salary, department_id)
-VALUES ('clothing dep stocker', 25000.00, 2);
+VALUES ('accountant', 42000.00, 2);
 
 INSERT INTO roles (title, salary, department_id)
-VALUES ('home dep stocker', 25000.00, 3);
+VALUES ('sales-person', 45000.00, 3);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ('warehouse-lackey', 40000.00, 4);
+
 
 -- managers
 INSERT INTO employees (first_name, last_name, role_id)
@@ -35,18 +50,18 @@ VALUES ('Michael', 'Scott', 1);
 INSERT INTO employees (first_name, last_name, role_id)
 VALUES ('Jim', 'Halpert', 2);
 
--- stockers
+-- employees
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ('Stanley', 'Hudson', 4, 1);
+VALUES ('Stanley', 'Hudson', 8, 1);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ('Oscar', 'Martinez', 4, 1);
+VALUES ('Oscar', 'Martinez', 4, 2);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ('Phyllis', 'Vance', 5, 2);
+VALUES ('Phyllis', 'Vance', 8, 1);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ('Andy', 'Bernard', 6, 2);
+VALUES ('Andy', 'Bernard', 8, 1);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ('Ryan', 'Howard', 6, 2);
+VALUES ('Ryan', 'Howard', 3, 2);
